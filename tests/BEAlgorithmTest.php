9398b81cbe1b9e4942aab8c031d123892d8b7ead
Thomas <thomas@lekoala.be>
2020-03-31 14:48:45 +0200

    fix release

--- tests/BEAlgorithmTest.php
+++ tests/BEAlgorithmTest.php
@@ -8,16 +8,14 @@ use PHPUnit\Framework\TestCase;
 class BEAlgorithmTest extends TestCase
 {
     const VALID_NUMBER = '00012511119';
-    const VALID_NUMBER2 = '00022911101'; // a valid number in year 2k
-    const VALID_NUMBER3 = '02073034044'; // a valid number after 2k
+    const VALID_NUMBER2 = '02073034044'; // a valid number in year 2k
     const INVALID_NUMBER_CHECK = '00012511120';
     const INVALID_NUMBER_LENGTH = '0001251112020';
 
     public function testValidNumber()
     {
-        $this->assertTrue(TINValid::checkTIN('be', self::VALID_NUMBER));
-        $this->assertTrue(TINValid::checkTIN('be', self::VALID_NUMBER2));
-        $this->assertTrue(TINValid::checkTIN('be', self::VALID_NUMBER3));
+        $this->assertTrue(TINValid::checkTIN('be', self::VALID_NUMBER), "Failed to validate " . self::VALID_NUMBER);
+        $this->assertTrue(TINValid::checkTIN('be', self::VALID_NUMBER2), "Failed to validate " . self::VALID_NUMBER2);
     }
 
     public function testInvalidNumber()
